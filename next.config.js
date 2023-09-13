915f27047b63450be599f17edbf048f90a05ef90
Moshood Olawale <shabimoshood07@gmail.com>
2023-09-13 16:09:18 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com", "www.uniabuja.edu.ng"],
+      },
+}
 
 module.exports = nextConfig
