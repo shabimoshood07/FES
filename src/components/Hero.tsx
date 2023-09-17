c54d3023a2c3ee4f728888b9efc3459b2ec7f221
unknown <shabimoshood07@gmail.com>
2023-09-17 10:57:02 +0100

    .

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import HeroText from "./HeroText";
 
 const Hero = () => {
   return (
     <div className="h-[calc(100vh-4rem)] bg-slate-500 text-slate-100">
-      <h1 className="text-4xl tracking-widest font-bold">Faculty of Environmental Sciences</h1>
+      {/* <h1 className="text-4xl tracking-widest font-bold">
+        Faculty of Environmental Sciences
+      </h1> */}
+      <HeroText text="Faculty of Environmental Sciences" />
     </div>
   );
 };
