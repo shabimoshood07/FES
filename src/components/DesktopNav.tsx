a3aa61ff5de087403784f35fedfea138f2972a3a
Moshood Olawale <shabimoshood07@gmail.com>
2024-01-11 07:17:53 +0100

    Fix: main logo

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -29,6 +29,8 @@ import {
 } from "@/components/ui/menubar";
 
 const DesktopNav = () => {
+  
+  
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   return (
     <Navbar
@@ -44,8 +46,8 @@ const DesktopNav = () => {
       <NavbarContent className="pr-3 mx-auto" justify="center">
         <NavbarBrand className="mx-auto ">
           <Image
-            // src="https://www.uniabuja.edu.ng/images/logo.png"
-            src="https://www.uniabuja.edu.ng/uploads/settings/16815763039597.png"
+            src="https://www.uniabuja.edu.ng/images/logo.png"
+            // src="https://www.uniabuja.edu.ng/uploads/settings/16815763039597.png"
             height={400}
             width={400}
             alt="uniabuja logo"
