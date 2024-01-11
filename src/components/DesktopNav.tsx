4ce4706034262b8abc696996dad1c074738ac612
Moshood Olawale <shabimoshood07@gmail.com>
2024-01-11 07:38:19 +0100

    Fix: main logo

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -46,8 +46,8 @@ const DesktopNav = () => {
       <NavbarContent className="pr-3 mx-auto" justify="center">
         <NavbarBrand className="mx-auto h-[70px]">
           <Image
-            // src="https://www.uniabuja.edu.ng/images/logo.png"
-            src="https://www.uniabuja.edu.ng/uploads/settings/16815763039597.png"
+            src="https://www.uniabuja.edu.ng/images/logo.png"
+            // src="https://www.uniabuja.edu.ng/uploads/settings/16815763039597.png"
             height={400}
             width={400}
             alt="uniabuja logo"
